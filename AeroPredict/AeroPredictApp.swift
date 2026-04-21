601f40759edd901a8c3a3cb4ddde05ea7e576dac
Hafsa Konain <hkonain@example.com>
2026-04-21 03:04:53 -0400

    Initial Commit

---
+++ AeroPredict/AeroPredictApp.swift
@@ -0,0 +1,17 @@
+//
+//  AeroPredictApp.swift
+//  AeroPredict
+//
+//  Created by Hafsa Konain on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct AeroPredictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
